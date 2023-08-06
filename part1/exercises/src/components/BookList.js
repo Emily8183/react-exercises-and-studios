9923b62b6b2b341ac50131db8e5ca786feac787d
Emily <emily.zy.lin@gmail.com>
2023-08-06 12:07:40 -0500

    finished part 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
    let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let book1 = "http://subs.booklistonline.com/custompages/Booklist/booklist_authenticator.aspx?publication=booklinks&edition_date=2021-11-02";
+   let book2 = "http://subs.booklistonline.com/custompages/Booklist/booklist_authenticator.aspx?publication=booklinks&edition_date=2022-03-15";
+   let book3 = "http://subs.booklistonline.com/custompages/Booklist/booklist_authenticator.aspx?publication=booklinks&edition_date=2021-01-26";
 
    return (
-      <div>
+      <>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
+         <img src={book1} alt="Book Links Nov 2021" />
+         <img src={book2} alt="Book Links March 2022" />
+         <img src={book3} alt="Book Links Feb 2021" />
+      </>      
    );
 }
