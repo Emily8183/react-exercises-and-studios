626debde33366a51f895601e289b25d2ca5cfc6a
Emily <emily.zy.lin@gmail.com>
2023-08-07 15:02:37 -0500

    finished Part 2 exercises

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,15 +1,35 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
 
+// const listItem =(props) =>{
+//    return props.map((ocean) => <div key = {props.id}><image src={props.image} alt={props.name} className="img" /></div>);
+//  }
+
+ const listItem = oceans.map (ocean =>
+   <div key={ocean.id} className={`${ocean.fishcheck === "true" ? "isAFish" : "profile"}`}>
+   <h1>Fun facts:</h1>
+   <ol>
+      <li>{ocean.fact1}</li>
+      <li>{ocean.fact2}</li>
+      <li>{ocean.fact3}</li>.
+   </ol>
+   <Button />
+   <img src={ocean.image} alt={ocean.name} className="img" />
+     
+   </div>
+);
 
 function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+{   
+   return(      
+         <ul>
+           {listItem}
+          
+         </ul>               
    );
 }
+
+
 
 export default Profile;
